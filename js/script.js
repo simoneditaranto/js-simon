23a4f4b79e7a120b700512ea81beba16a16d72cd
s.ditaranto <simodita99@gmail.com>
2024-02-09 18:53:36 +0100

    stampa a schermo tempo rimanente a fine giornata

--- js/script.js
+++ js/script.js
@@ -1,16 +1,32 @@
 // Sfruttiamo le timing functions per fare il conto alla rovescia per la correzione di domani!
 // Ogni secondo il nostro countdown dovrà scalare fino alle 9:30 di lunedì mattina!
 
-const d = new Date();
+setInterval(endDay, 1000);
+// let showCountdown = endDay();
 
-const countdown = new Date();
+// creo una funzione che mi calcola il tempo rimanente a mezzanotte di oggi
+function endDay() {
+    
+    let d = new Date();
 
-// provo a implementare prima un calcolatore che trova quanto manca alla mezzanotte di oggi
-countdown.setHours(24 - d.getHours());
-console.log("ore", countdown.getHours());
+    const countdown = new Date();
 
-countdown.setMinutes(60 - d.getMinutes());
-console.log("minuti", countdown.getMinutes());
 
-countdown.setSeconds(60 - d.getSeconds());
-console.log("seconedi", countdown.getSeconds());
+    // provo a implementare prima un calcolatore che trova quanto manca alla mezzanotte di oggi
+    countdown.setHours(24 - d.getHours());
+    hours = countdown.getHours();
+
+
+    countdown.setMinutes(60 - d.getMinutes());
+    minutes = countdown.getMinutes();
+
+
+    countdown.setSeconds(60 - d.getSeconds());
+    seconds = countdown.getSeconds();
+
+
+    document.getElementById("time-remaining").innerHTML = "ore " + countdown.getHours() + " minuti " + countdown.getMinutes() + " secondi " + countdown.getSeconds();
+    // console.log(timeRemaining);
+
+
+}
