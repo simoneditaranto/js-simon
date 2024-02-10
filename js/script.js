06121dbfebe4a3b82ed9a65fa677c4a614137fe3
s.ditaranto <simodita99@gmail.com>
2024-02-10 10:36:42 +0100

    stampa in pagina del tempo rimanente

--- js/script.js
+++ js/script.js
@@ -15,22 +15,21 @@ console.log("actualDay", actualDay);
 
 // attraverso la funzione getTime() che mi restituisce il tempo in millisecondi da una certa data al 1 gennaio 1970, calcolo la differenza tra le due date sopra
 // sarà il tempo in millisecondi che c'è tra oggi e la data successiva 
-const countdown = nextDay.getTime() - actualDay.getTime();
-console.log(countdown);
+// converto questo tempo utlizzando la funzione "msToTime" e trovo quanto manca
+const countdown = msToTime(nextDay.getTime() - actualDay.getTime());
 
-// converto questo tempo in millisecondi e trovo quanto manca
-console.log(msToTime(countdown)); 
+// scrivo in pagina il risultato
+document.getElementById("time-remaining").innerHTML = `Alla data: ${nextDay} <br> mancano ${countdown}`;
 
 
 
 // funzione che mi converte il tempo da millisecondi a "giorni : ore : minuti: secondi"
 function msToTime(duration) {
-    // var milliseconds = Math.floor((duration % 1000) / 100),
-    var seconds = Math.floor((duration / 1000) % 60),
-    minutes = Math.floor((duration / (1000 * 60)) % 60),
-    hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
-    day = Math.floor((duration / (1000 * 60 * 60 * 24)));
+    
+    const seconds = Math.floor((duration / 1000) % 60);
+    const minutes = Math.floor((duration / (1000 * 60)) % 60);
+    const hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
+    const day = Math.floor((duration / (1000 * 60 * 60 * 24)));
     
     return day + ":" + hours + ":" + minutes + ":" + seconds;
-    }
-// console.log(msToTime(57692300));
+}
